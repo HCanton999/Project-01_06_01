2594e8e93460f605adb17c45c875f5493a704ac4
Unknown <hcanton999@west-mec.org>
2018-08-21 15:30:11 -0700

    BEgan adding more validation

--- script.js
+++ script.js
@@ -22,24 +22,67 @@ function validateForm(evt) {
         evt.returnValue = false;
     }
     formValidity = true;
-    if (formValidity === true) {
+    validateRequired();
+
+    if (formValidity === true) {// form is valid 
+        document.getElementById("errorText").innerHTML = ""
+        document.getElementById("errprText").style.display = "none"
         document.getElementsByTagName("form")[0].submit();
+    }else {
+        document.getElementById("errorText").innerHTML = "Please fox the indicated problems and resubmit your order.";
+        document.getElementById("errorText").style.display = "block";
+        scroll(0,0);
     }
 }
 
 //making validation required 
 function validateRequired() {
-    
+    var inputField = document.querySelectorAll("#contactinfo" + " input");
+    var fieldsetValidity = true;
+    var currentElement;
+
+    try {
+        for (var i = 0; i < inputField.length; i++) {
+            currentElement = inputField[i];
+            if (currentElement.value === "") {
+                currentElement.style.background = "rgb(255,233,233)";
+                formValidity = false;
+            } else {
+                currentElement.style.background = "white";
+            }
+        }
+    } catch (error) {
+        
+    }
+}
+
+
+function generatePlaceholder() {
+    if (!Modernizr.input.placeholder) {
+        var addressBox = document.getElementById("addrinput");
+        addressBox.value = addressBox.placeholder;
+        addressBox.style.color = "rgb(178,184, 183)";
+        if (addressBox.addEventListener) {
+            addressBox.addEventListener("focus", zeroPlaceholder, false);
+            addressBox.addEventListener("blur", checkPlaceholder, false);
+        }else if (addressBox.attachEvent) {
+            addressBox.attachevent("onfocus", zeroPlaceholder);
+        } 
+    }
 }
 
 // A function to create listeners for functions that wil be used
-function createEventListeners() {
-    var form = document.getElementsByTagName("form")[0];
-    if (form.addEventListener) {
-        form.addEventListener("submit", validateForm, false);
-    } else if (form.attachEvent) {
-        form.attachEvent("onsubmit" , validateForm);
+function setUpPage() {
+    generatePlaceholder();
+    if (window.addEventListener) {
+        window.addEventListener("submit", validateForm, false);
+    } else if (window.attachEvent){
+        window.attachEvent("onsubmit", validateForm, false);
     }
 }
 // This is an event listener for the load event
-window.addEventListener("load", createEventListeners); 
+if (window.addEventListener) {
+    window.addEventListener("load", createEventListeners);
+} else if (window.attachEvent) {
+    window.attachEvent("onload", setUpPage);
+}
